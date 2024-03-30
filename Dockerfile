e0b4ca3ea737a601d7aad883e726ed5639b50928
User <alesban36@gmail.com>
2024-03-31 01:16:04 +0300

    Initial

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+﻿FROM mcr.microsoft.com/dotnet/aspnet:6.0 AS base
+WORKDIR /app
+EXPOSE 80
+EXPOSE 443
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["HiClass.API/HiClass.API.csproj", "HiClass.API/"]
+COPY ["HiClass.Application/HiClass.Application.csproj", "HiClass.Application/"]
+COPY ["HiClass.Domain/HiClass.Domain.csproj", "HiClass.Domain/"]
+COPY ["HiClass.Infrastructure/HiClass.Infrastructure.csproj", "HiClass.Infrastructure/"]
+COPY ["HiClass.Persistence/HiClass.Persistence.csproj", "HiClass.Persistence/"]
+
+RUN dotnet restore "HiClass.API/HiClass.API.csproj"
+COPY . .
+WORKDIR "/src/HiClass.API"
+RUN dotnet build "HiClass.API.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "HiClass.API.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "HiClass.API.dll"]
---
+++ flyway/sql/V2__set_default_values.sql
@@ -0,0 +1,132 @@
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('02f9fd4d-aa05-4e4c-8e34-767c98d27f12', 'Music');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('05e6b1b1-3b12-49f7-bea9-753fa0eeae40', 'Spanish as a foreign language');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('0da2ba3c-3abf-4de4-bbe8-c09aa70a722c', 'Russian literature');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('0fb4714f-5d2e-42dc-8706-2c21b4e4cbed', 'Italian as a foreign language');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('17a6613d-8bd0-42b7-bffc-6114974263ef', 'World art');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('17f8c45e-d4cc-4b08-8a48-49dd1979fed9', 'Cultural exchange');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('1b8a10a5-1a83-4278-a9bb-a979d29e3935', 'Biology');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('1f1b2960-331c-4755-a1e3-f097f9785918', 'Chinese as a foreign language');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('2801d8b9-4779-45d8-b78e-1052d5dff390', 'Russian language');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('28e3c202-9f77-4805-87a5-02b877f7bd6a', 'Economics');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('2e584404-3842-472e-a9c7-825ef3ed1dae', 'Crafts');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('3ed5c40c-2f75-46d6-a4b0-6dd4242291d7', 'History');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('43af8d52-9008-4cb2-9739-ec79e303c17b', 'Computer science');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('5a7b8fbc-bc5d-4376-9a0e-17b6304f9fb7', 'French as a foreign language');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('807135f9-2abe-4ef4-b120-47d449e47ecb', 'Technology');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('8e19aa36-125f-4e27-a901-56d2eb8ce365', 'Astronomy');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('b2dfcae6-e168-4edf-ba86-c6956f083f65', 'Mathematics');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('b77465f3-31f5-4eb9-95f0-deb670109de4', 'Natural science');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('ba4acc22-4d91-4227-bf36-9d89886c61b6', 'German as a foreign language');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('c102990f-c143-47ab-8fa7-b8ce88f3dffa', 'Project-based learning');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('d085439d-8fd5-409b-8b3b-9d1361e6eac1', 'Social science');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('d3c5cb30-60cd-4ac8-84c8-d4915d6fdfb6', 'Fine arts');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('d63c58b7-a4c4-4930-b127-8e9718e30bc9', 'Chemistry');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('d74ce94c-33f2-4ede-83e5-2cc496e1a219', 'Vacation education');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('dabc27fe-423c-49d9-80f3-840ee8536e15', 'English as a foreign language');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('dc4c37b2-ca2c-41f9-82f2-905eeb2717c4', 'Physics');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('fadf1fa0-6135-43ae-b7f8-a10ad24955ea', 'Regional studies');
+INSERT INTO "Disciplines" ("DisciplineId", "Title")
+VALUES ('ffd7a8a9-593e-4d82-b7a0-e3b4b041eb08', 'Geography');
+
+INSERT INTO "Grades" ("GradeId", "GradeNumber")
+VALUES ('033371db-6999-46b3-8f64-303cd7612527', 9);
+INSERT INTO "Grades" ("GradeId", "GradeNumber")
+VALUES ('09cbfeeb-ec18-479d-805c-c7e6a52bc150', 8);
+INSERT INTO "Grades" ("GradeId", "GradeNumber")
+VALUES ('13cee73a-4363-42c1-99a4-72e14a99b9b1', 7);
+INSERT INTO "Grades" ("GradeId", "GradeNumber")
+VALUES ('1b356a60-b9de-4f01-acbb-61f11625fab0', 3);
+INSERT INTO "Grades" ("GradeId", "GradeNumber")
+VALUES ('6bd17d30-5f2a-4c96-b037-69ce8e79d98b', 5);
+INSERT INTO "Grades" ("GradeId", "GradeNumber")
+VALUES ('7de4568d-e1f0-48e4-b0e8-3c8400ec149d', 10);
+INSERT INTO "Grades" ("GradeId", "GradeNumber")
+VALUES ('93c87b49-72ba-4b09-9f41-58fd420df813', 2);
+INSERT INTO "Grades" ("GradeId", "GradeNumber")
+VALUES ('99b2705e-716c-4180-a98f-8bd3e406d4b6', 4);
+INSERT INTO "Grades" ("GradeId", "GradeNumber")
+VALUES ('a114d41c-4ab0-4690-ab41-1603c3702bbc', 11);
+INSERT INTO "Grades" ("GradeId", "GradeNumber")
+VALUES ('a769cc39-9cb3-4f34-a6f9-f4e2895b78ab', 12);
+INSERT INTO "Grades" ("GradeId", "GradeNumber")
+VALUES ('ddc0ea18-ae33-4261-b9e3-5866f2fe0aed', 6);
+INSERT INTO "Grades" ("GradeId", "GradeNumber")
+VALUES ('ea12082a-94b1-4910-b4a9-cc3ac44c6a01', 1);
+
+INSERT INTO "InstitutionTypes" ("InstitutionTypeId", "Title")
+VALUES ('650faced-5792-4e41-b9a4-c908103872f8', 'Lyceum');
+INSERT INTO "InstitutionTypes" ("InstitutionTypeId", "Title")
+VALUES ('7dda2b8b-d9aa-418f-ad52-0f2c94732192', 'School');
+INSERT INTO "InstitutionTypes" ("InstitutionTypeId", "Title")
+VALUES ('83a9f0b0-c5be-48da-8ceb-d310aeb0db51', 'Gymnasium');
+INSERT INTO "InstitutionTypes" ("InstitutionTypeId", "Title")
+VALUES ('8d9bb9d6-ccb3-41c6-b6ee-7dc1b1b9b11e', 'College');
+
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('1024a0ac-29d1-49a3-9cf4-647e8b76b21a', 'Spanish');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('116e30a2-cf1e-4734-91e4-9e933d0a7808', 'French');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('1bf3f231-a1c6-442b-9096-46677e54cf6d', 'Russian');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('3d60e2ca-7770-4ae0-a73c-56f5328add4d', 'Hungarian');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('48c9b4b7-6c22-4714-a77c-24d6bc47a840', 'Italian');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('51234572-b208-4d69-82e8-0dda40f8b6b7', 'Georgian');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('56a6ede4-30cc-4280-b86a-5301d3309856', 'Portuguese');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('65a2b066-412b-4a30-8258-96c52353b268', 'English');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('73e0e619-81a1-4849-94cb-6115754c8eea', 'Uzbek');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('8b87d2a5-205a-4302-b38b-a8fc27f5cec3', 'Azerbaijani');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('9c6143b4-2f54-4137-b985-6eec99813e69', 'Tajik');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('9eaab33a-acd8-44ad-80f9-bbf23b43d262', 'Ukrainian');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('a2b015b8-f631-453e-811d-703866ac4569', 'Belarusian');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('c5c92640-8058-4131-9fd9-50ae925b4a96', 'Kazakh');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('ed5247e2-4c0c-4c83-9189-4378a0b3aec6', 'Kyrgyz');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('ee1df58e-12d3-42ab-8436-1e5af119a94a', 'German');
+INSERT INTO "Languages" ("LanguageId", "Title")
+VALUES ('fdda94ee-0118-442c-8ba4-803f3db09d97', 'Armenian');
+
+INSERT INTO "Roles" ("RoleId", "Title")
+VALUES ('26e03c89-130d-4747-933c-31b1eb701a8e', 'User');
+INSERT INTO "Roles" ("RoleId", "Title")
+VALUES ('a01c731b-2c43-4d48-9b77-2adf7fdf1b2b', 'Manager');
+INSERT INTO "Roles" ("RoleId", "Title")
+VALUES ('f75e0754-f086-4e54-8e67-18f683b7a01e', 'Admin');
